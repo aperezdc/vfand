4178910b0356e04ca56d5ee056bb073b949525bf
Adrian Perez <aperez@igalia.com>
2010-07-28 19:15:17 +0200

    Reindent to follow guidelines (4 spaces indent)

--- vfand.c
+++ vfand.c
@@ -69,9 +69,9 @@ static unsigned    temp_low     = TEMP_LOW;
 	"\n" \
 	"  -d DEVICE   Path to device (default: " SONYPI_PATH ")\n" \
 	"  -i SECONDS  Polling interval, in seconds (default: " S_(POLL_INTERVAL) ")\n" \
-  "  -H DEGREES  Temperature considered \"high\" (default: " S_(TEMP_HIGH) ")\n" \
-  "  -L DEGREES  Temperature considered \"low\" (default: " S_(TEMP_LOW) ")\n" \
-  "  -v          Print informational messages.\n" \
+	"  -H DEGREES  Temperature considered \"high\" (default: " S_(TEMP_HIGH) ")\n" \
+	"  -L DEGREES  Temperature considered \"low\" (default: " S_(TEMP_LOW) ")\n" \
+	"  -v          Print informational messages.\n" \
 	"  -h, -?      Show this help text.\n" \
 	"\n"
 
@@ -88,60 +88,60 @@ static unsigned    temp_low     = TEMP_LOW;
 
 static void
 report_data (unsigned temp_low,
-		         unsigned temp_high,
-		         unsigned temp_cur,
-		         unsigned speed_min,
-		         unsigned speed_max,
-		         unsigned speed_cur,
-		         unsigned speed_new)
+             unsigned temp_high,
+             unsigned temp_cur,
+             unsigned speed_min,
+             unsigned speed_max,
+             unsigned speed_cur,
+             unsigned speed_new)
 {
-	(void) temp_low;  /* unused */
-	(void) temp_high; /* unused */
-	(void) speed_min; /* unused */
-	(void) speed_max; /* unused */
+    (void) temp_low;  /* unused */
+    (void) temp_high; /* unused */
+    (void) speed_min; /* unused */
+    (void) speed_max; /* unused */
 
-	printf ("temp %3u C, speed %3u, new speed %3u\n",
-			    temp_cur, speed_cur, speed_new);
+    printf ("temp %3u C, speed %3u, new speed %3u\n",
+            temp_cur, speed_cur, speed_new);
 }
 
 
 static int
 fanspeed_calculate (unsigned temp_low,
-		                unsigned temp_high,
-		                unsigned temp_cur,
-		                unsigned speed_min,
-		                unsigned speed_max,
-		                unsigned speed_cur)
+                    unsigned temp_high,
+                    unsigned temp_cur,
+                    unsigned speed_min,
+                    unsigned speed_max,
+                    unsigned speed_cur)
 {
-	double slope;
-	unsigned result;
+    double slope;
+    unsigned result;
 
-	assert (temp_low < temp_high);
-	(void) speed_cur; /* unused */
+    assert (temp_low < temp_high);
+    (void) speed_cur; /* unused */
 
-	if (temp_cur < temp_low) {
-		result = speed_max;
-	}
-	else if (temp_cur > temp_high) {
-		result = speed_max;
-	}
-	else {
-		/* Calculate using a linear formula */
-		slope = (double) (speed_max - speed_min) / (temp_high - temp_low);
-		result = (unsigned) slope * (temp_cur - temp_low) + speed_min;
-	}
+    if (temp_cur < temp_low) {
+        result = speed_max;
+    }
+    else if (temp_cur > temp_high) {
+        result = speed_max;
+    }
+    else {
+        /* Calculate using a linear formula */
+        slope = (double) (speed_max - speed_min) / (temp_high - temp_low);
+        result = (unsigned) slope * (temp_cur - temp_low) + speed_min;
+    }
 
-	if (verbose) {
-		report_data (temp_low,
-				         temp_high,
-				         temp_cur,
-				         speed_min,
-				         speed_max,
-				         speed_cur,
-				         result);
-	}
+    if (verbose) {
+        report_data (temp_low,
+                     temp_high,
+                     temp_cur,
+                     speed_min,
+                     speed_max,
+                     speed_cur,
+                     result);
+    }
 
-	return result;
+    return result;
 }
 
 
@@ -149,115 +149,115 @@ fanspeed_calculate (unsigned temp_low,
 void
 sonypi_fanspeed_set (int fd, unsigned value)
 {
-	__u8 value8;
+    __u8 value8;
 
-	if (value >= FANSPEED_MAX)
-		value = FANSPEED_MAX;
-	if (value <= FANSPEED_MIN)
-		value = FANSPEED_MIN;
+    if (value >= FANSPEED_MAX)
+        value = FANSPEED_MAX;
+    if (value <= FANSPEED_MIN)
+        value = FANSPEED_MIN;
 
-	value8 = (__u8) value;
+    value8 = (__u8) value;
 
-	if (ioctl (fd, SONYPI_IOCSFAN, &value8) < 0) {
-		fprintf (stderr, "ioctl: %s\n", strerror (errno));
-	}
+    if (ioctl (fd, SONYPI_IOCSFAN, &value8) < 0) {
+        fprintf (stderr, "ioctl: %s\n", strerror (errno));
+    }
 }
 
 
 unsigned
 sonypi_fanspeed_get (int fd)
 {
-	__u8 value;
-	if (ioctl (fd, SONYPI_IOCGFAN, &value) < 0) {
-		fprintf (stderr, "ioctl: %s\n", strerror (errno));
-	}
-	return (unsigned) value;
+    __u8 value;
+    if (ioctl (fd, SONYPI_IOCGFAN, &value) < 0) {
+        fprintf (stderr, "ioctl: %s\n", strerror (errno));
+    }
+    return (unsigned) value;
 }
 
 
 unsigned
 sonypi_temperature_get (int fd)
 {
-	__u8 value;
-	if (ioctl (fd, SONYPI_IOCGTEMP, &value) < 0) {
-		fprintf (stderr, "ioctl: %s\n", strerror (errno));
-	}
-	return (unsigned) value;
+    __u8 value;
+    if (ioctl (fd, SONYPI_IOCGTEMP, &value) < 0) {
+        fprintf (stderr, "ioctl: %s\n", strerror (errno));
+    }
+    return (unsigned) value;
 }
-
-
 
 
 
 int
 main (int argc, char **argv)
 {
-	char *endpos;
-	unsigned remain;
-	int c;
+    char *endpos;
+    unsigned remain;
+    int c;
 
-	while ((c = getopt (argc, argv, "?hvd:i:H:L:")) != -1) {
-		switch (c) {
-			case 'v':
-				verbose = 1;
-				break;
-			case 'd':
-				sonypi_path = optarg;
-				break;
-			case 'i':
-				_unsigned_arg (interval);
-				break;
-			case 'H':
-				_unsigned_arg (temp_high);
-				break;
-			case 'L':
-				_unsigned_arg (temp_low);
-				break;
-			case '?':
-			case 'h':
-				printf (_vfand_help_message, argv[0]);
-				exit (EXIT_SUCCESS);
-			default:
-				fprintf (stderr, "%s: unrecognized option '-%c'\n", argv[0], c);
-				fprintf (stderr, _vfand_help_message, argv[0]);
-				exit (EXIT_FAILURE);
-		}
-	}
+    while ((c = getopt (argc, argv, "?hvd:i:H:L:")) != -1) {
+        switch (c) {
+            case 'v':
+                verbose = 1;
+                break;
+            case 'd':
+                sonypi_path = optarg;
+                break;
+            case 'i':
+                _unsigned_arg (interval);
+                break;
+            case 'H':
+                _unsigned_arg (temp_high);
+                break;
+            case 'L':
+                _unsigned_arg (temp_low);
+                break;
+            case '?':
+            case 'h':
+                printf (_vfand_help_message, argv[0]);
+                exit (EXIT_SUCCESS);
+            default:
+                fprintf (stderr, "%s: unrecognized option '-%c'\n", argv[0], c);
+                fprintf (stderr, _vfand_help_message, argv[0]);
+                exit (EXIT_FAILURE);
+        }
+    }
 
-	c = O_RDWR;
+    c = O_RDWR;
 
 #ifdef O_CLOEXEC
-	c |= O_CLOEXEC;
+    c |= O_CLOEXEC;
 #endif /* O_CLOEXEC */
 
-	if ((sonypi_fd = open (sonypi_path, c, 0)) == -1) {
-		fprintf (stderr, "%s: cannot open '%s': %s\n", argv[0],
-				sonypi_path, strerror(errno));
-		exit (EXIT_FAILURE);
-	}
+    if ((sonypi_fd = open (sonypi_path, c, 0)) == -1) {
+        fprintf (stderr, "%s: cannot open '%s': %s\n", argv[0],
+                 sonypi_path, strerror(errno));
+        exit (EXIT_FAILURE);
+    }
 
-	while (interval > 0) {
-		/* Adjust */
-		sonypi_fanspeed_set (sonypi_fd,
-		                     fanspeed_calculate (temp_low,
-													                   temp_high,
-													                   sonypi_temperature_get (sonypi_fd),
-													                   FANSPEED_MIN,
-													                   FANSPEED_MAX,
-				                                     sonypi_fanspeed_get (sonypi_fd)));
+    while (interval > 0) {
+        /* Adjust */
+        sonypi_fanspeed_set (sonypi_fd,
+                             fanspeed_calculate (temp_low,
+                                                 temp_high,
+                                                 sonypi_temperature_get (sonypi_fd),
+                                                 FANSPEED_MIN,
+                                                 FANSPEED_MAX,
+                                                 sonypi_fanspeed_get (sonypi_fd)));
 
-		/* Wait */
-		remain = interval;
-		do {
-			remain = sleep (remain);
-		} while (remain > 0 && errno == EINTR);
-	}
+        /* Wait */
+        remain = interval;
+        do {
+            remain = sleep (remain);
+        } while (remain > 0 && errno == EINTR);
+    }
 
-	if (sonypi_fd != -1) {
-		close (sonypi_fd);
-	}
+    if (sonypi_fd != -1) {
+        close (sonypi_fd);
+    }
 
-	exit (EXIT_SUCCESS);
+    exit (EXIT_SUCCESS);
 }
 
 
+/* vim: expandtab tabstop=4 shiftwidth=4
+ */
